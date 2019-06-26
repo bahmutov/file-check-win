185c4f8d52d933bf0911fb68bf74c50f8d3dc870
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-06-26 15:56:50 -0400

    try passing via ENV variable

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const fs = require('fs-extra')
 
 // which is this __filename
-const filename = 'C:\\projects\\file-check-win\\index.js'
+// const filename = 'C:\\projects\\file-check-win\\index.js'
+const filename = process.env.FILE || __filename
 console.log('filename is %s', filename)
-fs.pathExists(filename).then(console.log).catch(e => {
-  console.error('caught error', e)
-})
+fs.pathExists(filename)
+  .then(console.log)
+  .catch(e => {
+    console.error('caught error', e)
+  })
