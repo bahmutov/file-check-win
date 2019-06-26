4fb17242768127c5a4633e7f65fdd86d0784008c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-06-26 16:02:38 -0400

    hmm no single quotes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs-extra')
 console.log('__filename is %s', __filename)
 
 // const filename = 'C:\\projects\\file-check-win\\index.js'
-const filename = process.env.FILE || __filename
+const filename = process.env.FILE
 console.log('filename is %s', filename)
 fs.pathExists(filename)
   .then(console.log)
